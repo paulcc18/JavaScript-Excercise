282228d97583feaaf9df007c2ebf90eceddb6d34
paulcc18 <gpc1808@gmail.com>
2024-06-18 11:00:09 -0500

    I upload the code

---
+++ script.js
@@ -0,0 +1,12 @@
+function onClickSubmitButton(){
+  let inputvalue = document.getElementById('InputText').value;
+  if (inputvalue>0){
+    alert('Hey el numero es mayor que cero')
+  }
+  if (inputvalue<0){
+    alert('hey el numero es menor que cero')
+  }
+  if (inputvalue==0){
+    alert('Intente otra vez')
+  }
+}
